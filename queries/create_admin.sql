cf4d444533c982f8ebe3ae0b42abf9794e23d921
bhp <bhp@bhps-Mac-mini.tampabay.rr.com>
2023-07-08 06:18:17 -0400

    add queries

---
+++ queries/create_admin.sql
@@ -0,0 +1,3 @@
+update users
+set role_id = 1
+where username = 'username'
---
+++ queries/drop_tables.sql
@@ -0,0 +1,5 @@
+drop table alembic_version;
+drop table roles_permissions;
+drop table permissions;
+drop table users;
+drop table roles;
